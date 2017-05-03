e973ec6f509f657524a60ec18559b15b240714a3
Justin Barclay <justincbarclay@gmail.com>
2017-05-02 20:53:55 -0600

    Copy user code from AHAS as basis for users in Party

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,152 @@
+# Controller for interacting with Administrative duties including User creation and Deletion
+# This is a protected class and requires the user to login before it can access any of the functions
+#
+#
+# @author Justin Barclay & Mackenzie Bligh
+# @see https://github.com/CMPUT401/vettr_server/wiki/API-Documentation#user_authentication
+class UsersController < ApplicationController
+  # before_action :authenticate_user
+  # skip_before_action :authenticate_user, only: [:create]
+  
+  # Handles POST request to create a new user, route /api/signup
+  # This is an unprotected route.
+  # @example
+  #   {
+  #     "user": 
+  #      { 
+  #        "name": "User McUser",
+  #        "email": "user@example.com",
+  #        "password": "bazfoobar",
+  #        "password_confirmation": "bazfoobar" 
+  #      }
+  #   }
+  # @example success
+  #   {
+  #     "success": true
+  #   }
+  # @example failure
+  #   {
+  #     "success": false
+  #     "errors": [....]
+  #   }
+  # @return HTTP 201 JSON, on success
+  # @return HTTP 500 JSON, on failure
+  def create
+    updated_user = user_params
+    # validate token
+    @user = User.find_by(invite_token: updated_user[:invite_token])
+    if updated_user[:invite_token].nil?
+      render status: :error, json: { success: false, error: 'Invalid token' }
+      return
+    end
+    if @user.nil?
+      render status: 404, json: { success: false, error: 'Unable to find user' }
+      return
+    end
+    # Ensure matching passwords
+    
+    if updated_user[:password] == updated_user[:password_confirmation]
+      @user.password = updated_user[:password]
+      @user.invite_token = nil
+    else
+      render status: :error, json: { success: false, error: 'Passwords don\'t match' }
+      return
+    end
+    # Update password and make sure if matches our standards
+    if @user.save
+      render status: :created, json: { success: true }
+    else
+      render status: :error, json: { success: false, errors: @user.errors.full_messages }
+    end
+  end
+
+  # Handles GET request to show user info during signup, route /users/:token
+  # This is an unprotected route.
+  # @example success
+  #   {
+  #     "user": 
+  #      { 
+  #        "name": "User McUser",
+  #        "email": "user@example.com",
+  #        "id": "integer"
+  #      }
+  #   }
+  #  # @example failure
+  #   {
+  #     "success": false
+  #     "errors": "User not found"
+  #   }
+  # @return HTTP 200 JSON, on success
+  # @return HTTP 404 JSON, on failure
+  def show
+    # Display a user based on an invite token
+    # This might need to be refactored based on being able to update a user
+    user = User.find_by(invite_token: params[:token])
+    if user
+      render status: 200, json: { success: true, user: filter_user(user) }
+    else
+      render status: 404, json: { success: false, error: 'Unable to find user' }
+    end
+  end
+  
+  # Handle POST request to reset a users password, route reset_password
+  # @example
+  #  {
+  #     "user":
+  #         { 
+  #             "password": "strint",
+  #             "password_confirmation": "string"
+  #             "reset_token": "string"
+  #         }
+  #  }
+  # @return HTTP 201 JSON, on Success
+  # @return HTTP 500 JSON, on Failure
+  def reset_password
+    updated_password = password_params
+
+    # validate token
+    if updated_password[:reset_token].nil?
+      render status: :error, json: { success: false, error: 'Invalid token' }
+      return
+    end
+    @user = User.find_by(reset_token: updated_password[:reset_token])
+    if @user.nil?
+      render status: 404, json: { success: false, error: 'Unable to find user' }
+      return
+    end
+    # Ensure matching passwords
+    if updated_password[:password] == updated_password[:password_confirmation]
+      @user.password = updated_password[:password]
+      @user.reset_token = nil
+    else
+      render status: :error, json: { success: false, error: 'Passwords don\'t match' }
+      return
+    end
+    # Update password and make sure if matches our standards
+    if @user.save
+      render status: :created, json: { success: true }
+    else
+      render status: :error, json: { success: false, errors: @user.errors.full_messages }
+    end
+  end
+  
+  private
+
+  def filter_users(users)
+    users.map do |user|
+      { id: user.id, email: user.email, name: user.name }
+    end
+  end
+
+  def filter_user(user)
+    { email: user.email, name: user.name }
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation, :reset_token)
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation, :invite_token)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
+#
+
+class User < ApplicationRecord
+  has_secure_password
+  # Ensure that the users email is unique by making all emails lowercase
+  before_save { email.downcase! }
+
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
+  
+  validates :password, presence: true, length: { minimum: 7 }, allow_nil: true
+
+  # digest user password with specific Bcrypt parameters
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.from_token_payload(payload)
+    # Returns a valid user, `nil` or raise
+    # e.g.
+    self.find payload["sub"]
+  end
+
+  def to_token_payload
+    { sub: id, role: type }
+  end
+end
