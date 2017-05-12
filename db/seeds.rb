981d0643564ab84430f68adfd8ccde56456fe66f
Justin Barclay <justincbarclay@gmail.com>
2017-05-12 14:05:25 -0600

    Add seed data for easy testing, remember to remove seed later

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(first_name: 'Justin', last_name: 'Barclay', email: 'justincbarclay@gmail.com', password: 'password', password_confirmation: 'password')
+part = Part.new(name: :Gasket, room: 'Mechanical Room', shelf: :A3, count: 204)
+
+# not exactly sure how party history works
+part.save
+user.save
+
+PartHistory.new(user: user, part: part, change: 10).save
+PartHistory.new(user: user, part: part, change: -15).save
+PartHistory.new(user: user, part: part, change: 7).save
