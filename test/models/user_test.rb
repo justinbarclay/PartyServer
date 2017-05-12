671d53361f841ca6b65a95f4e6abc27eb4880851
Justin Barclay <justincbarclay@gmail.com>
2017-05-11 19:42:41 -0600

    Add user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(first_name: "Justin", last_name: "Barclay", email: "test@example.com", password: "foobarbaz", password_confirmation: "foobarbaz")
+  end
+  
+  test "normal user model can be saved" do
+    assert @user.valid?
+    assert @user.save
+  end
+
+  test "user with a short password" do
+     @user = User.new(first_name: "Justin", last_name: "Barclay", email: "test@example.com", password: "fooba", password_confirmation: "fooba")
+     assert_not @user.valid?
+  end
+
+  test "user with mismatched password and password confirmation" do
+    @user.password_confirmation = "thing"
+    assert_not @user.valid?
+  end
+
+  test "user with an incorrect email" do
+    @user.email = "thing@.co"
+    assert_not @user.valid?
+  end
 end
