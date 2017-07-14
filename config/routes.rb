6ee443fb92c516cab03c96e9b84e271622c87c3e
Justin Barclay <justincbarclay@gmail.com>
2017-07-14 15:07:04 -0600

    Add catch all route and redirect to root

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,6 @@ Rails.application.routes.draw do
       post         'login',                   to: 'user_token#create'
     end
   end
+
+  get          '/*path',                      to: redirect('/')
 end
